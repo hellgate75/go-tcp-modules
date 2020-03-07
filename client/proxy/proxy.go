94c4c7c5c12991895b42e451720c38d74d102e68
hellgate75 <hellgate75@gmail.com>
2020-03-07 10:47:33 +0000

    Adding client modules

---
+++ client/proxy/proxy.go
@@ -0,0 +1,45 @@
+package proxy
+
+import (
+	"errors"
+	"fmt"
+	"github.com/hellgate75/go-tcp-modules/client/proxy/shell"
+	"github.com/hellgate75/go-tcp-modules/client/proxy/transfer"
+	"github.com/hellgate75/go-tcp-client/common"
+	"github.com/hellgate75/go-tcp-client/log"
+)
+
+var sendersMap map[string]common.Sender = make(map[string]common.Sender)
+var Logger log.Logger = nil
+var filled bool = false
+
+
+
+func initMap() {
+	sendersMap["transfer-file"] = transfer.New()
+	sendersMap["shell"] = shell.New()
+	filled = true
+}
+
+func GetSender(command string) (common.Sender, error) {
+	if !filled {
+		initMap()
+	}
+	if sender, ok := sendersMap[command]; ok {
+		sender.SetLogger(Logger)
+		return sender, nil
+	} else {
+		return nil, errors.New(fmt.Sprintf("Sender unavailable: %s", command))
+	}
+}
+
+func Help() []string {
+	if !filled {
+		initMap()
+	}
+	var list []string = make([]string, 0)
+	for _, sender := range sendersMap {
+		list = append(list, sender.Helper())
+	}
+	return list
+}
