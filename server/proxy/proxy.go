fc07ebcd7449cc502427fcfe4df96bbb5d06e3a8
hellgate75 <hellgate75@gmail.com>
2020-03-07 10:40:16 +0000

    Adding server modules

---
+++ server/proxy/proxy.go
@@ -0,0 +1,35 @@
+package proxy
+
+import (
+	"errors"
+	"fmt"
+	"github.com/hellgate75/go-tcp-server/common"
+	"github.com/hellgate75/go-tcp-modules/server/proxy/shell"
+	"github.com/hellgate75/go-tcp-modules/server/proxy/transfer"
+	"strings"
+)
+
+var commandsMap map[string]common.Commander = make(map[string]common.Commander)
+
+var filled bool = false
+
+func initMap() {
+	commandsMap["transfer-file"] = transfer.New()
+	commandsMap["shell"] = shell.New()
+	filled = true
+}
+
+func GetCommander(command string) (common.Commander, error) {
+	if !filled {
+		initMap()
+	}
+	var cmd string = strings.TrimSpace(strings.ToLower(command))
+	if commander, ok := commandsMap[cmd]; ok {
+		if commander == nil {
+			return nil, errors.New(fmt.Sprintf("Unable to collect command: %s", cmd))
+		}
+		return commander, nil
+	} else {
+		return nil, errors.New(fmt.Sprintf("Command unavailable: %s", cmd))
+	}
+}
